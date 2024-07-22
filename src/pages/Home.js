2703254d6d4903886c5f0271ac701403e92587b7
Noah Turpin <noahhturpinn@gmail.com>
2024-07-21 20:35:17 -0400

    final final touches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div className="home">
-        <h2> Make brackets and predictions all in one place <span>Bracketzone</span>
+        <h2> Make brackets and predictions all in one place with <span>BracketZone</span>
         </h2>
         
         <img src="https://images.wsj.net/im-738124?width=1280&size=1" alt="connor macdavid" />
